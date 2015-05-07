88bd0751b0c6c5b9737cd096473b4ce27b4353b0
David Bellingroth <spam.dbellingroth@gmail.com>
2015-05-07 15:24:21 +0200

    wiki watchdog implemented

--- scripts/miwiki.js
+++ scripts/miwiki.js
@@ -10,6 +10,30 @@
 // Commands:
 //   hubot wiki status  - shows the status of the Medieninformatik Wiki
 module.exports = function(robot) {
+
+	var wiki_room = 'wiki';
+	var wiki_status = null;
+	var interval = setInterval(function(){
+		
+		robot.http('https://www.medieninformatik.fh-koeln.de/w/').get()(function(err, res, body){
+			if (!err && res.statusCode == 200) {
+				if (wiki_status == 'down') {
+					robot.messageRoom(wiki_room, 'The wiki is up again 😊!');
+					wiki_status = 'up';
+				}
+			}
+			else {
+				if (wiki_status != 'down') {
+					robot.messageRoom(wiki_room, 'The wiki is down 😱!');
+					wiki_status = 'down';
+				}	
+			}
+		});
+
+
+	}, 5*60000);
+
+	
     robot.respond(/wiki status/i, function(msg){
 		robot.http('https://www.medieninformatik.fh-koeln.de/w/').get()(function(err, res, body){
 			if (!err && res.statusCode == 200) {
